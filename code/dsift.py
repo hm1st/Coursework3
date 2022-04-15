58a935565b1feaec6b7f3f7bbb7868240cd7b8c7
31170 <3117046103@qq.com>
2022-04-15 22:43:31 +0100

    1

--- code/dsift.py
+++ code/dsift.py
@@ -4,5 +4,6 @@ import numpy as np
 def get_dsift(image):
     if image.size != (256,256):
         image = image.resize((256,256))
-    _, descriptors = cyvlfeat.sift.dsift(np.array(image), step=[4,4], fast=True)
+    _, descriptors = cyvlfeat.sift.dsift(np.array(image), step=[16,16], fast=True)
     return descriptors
+
--- code/train_vocab_3.py
+++ code/train_vocab_3.py
@@ -1,22 +1,36 @@
 from category_path import get_category, get_path
 from dsift import get_dsift
+import torch
+# from tqdm import tqdm
+from kmeans_pytorch import kmeans
 from sklearn.cluster import KMeans
 import numpy as np
 from PIL import Image
-
 # Parameters
 NUM_CLUSTERS = 500
 
 category = get_category()
-kmeans = KMeans(n_clusters=NUM_CLUSTERS)
+flag =0
 for c in category:
     train_path = get_path('train', c)
     for path in train_path:
         image = Image.open(path)
         dsift = get_dsift(image)
         dsift = np.nan_to_num(dsift)
-        kmeans.fit(dsift)
+        if flag ==0:
+            data = np.zeros(shape=(dsift.shape[0],dsift.shape[1]))
+            data[:,:] = dsift
+            flag = 1
+        else:
+            data = np.append(data,dsift,axis=0)
         del dsift
     print('{} Completed!'.format(c))
-vocab = kmeans.cluster_centers_
-np.save('vocab_dsift.npy', vocab)
+
+print(data.shape)
+data = torch.tensor(data[np.random.choice(range(data.shape[0]), int(np.floor(0.1*data.shape[0])), replace=False), :])
+cluster_ids_x, cluster_centers= kmeans(X=data,num_clusters=NUM_CLUSTERS,device=torch.device('cuda:0'))
+
+# kmeans = KMeans(n_clusters=NUM_CLUSTERS).fit(data.reshape((nx*ny,nz)))
+
+
+np.save('vocab_dsift.npy', cluster_centers)
